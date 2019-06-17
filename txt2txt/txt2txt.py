6a8e68db42e6568a215cfd672869aa76e400a60f
Praneeth <praneethbedapudi@gmail.com>
2019-06-18 00:44:56 +0530

    add batch inputs to beam search(generate function)

--- txt2txt/txt2txt.py
+++ txt2txt/txt2txt.py
@@ -49,56 +49,93 @@ def decode_sequence(decoding_dict, sequence):
     return text
 
 
-def generate(text, input_encoding_dict, model, max_input_length, max_output_length, beam_size, max_beams, min_cut_off_len, cut_off_ratio):
-    min_cut_off_len = max(min_cut_off_len, cut_off_ratio*len(text))
+def generate(texts, input_encoding_dict, model, max_input_length, max_output_length, beam_size, max_beams, min_cut_off_len, cut_off_ratio):
+    if not isinstance(texts, list):
+        texts = [texts]
+
+    min_cut_off_len = max(min_cut_off_len, cut_off_ratio*len(max(texts, key=len)))
     min_cut_off_len = min(min_cut_off_len, max_output_length)
 
-    completed_beams = []
-    running_beams = [
-        [np.zeros(shape=(len(text), max_output_length)), [1]]
-        ]
-    running_beams[0][0][:,0] = char_start_encoding
+    all_completed_beams = {i:[] for i in range(len(texts))}
+    all_running_beams = {}
+    for i, text in enumerate(texts):
+        all_running_beams[i] = [[np.zeros(shape=(len(text), max_output_length)), [1]]]
+        all_running_beams[i][0][0][:,0] = char_start_encoding
+
     
-    while len(running_beams) != 0:
-        running_beams = sorted(running_beams, key=lambda tup:np.prod(tup[1]), reverse=True)
-        running_beams = running_beams[:max_beams]
+    while len(all_running_beams) != 0:
+        for i in all_running_beams:
+            all_running_beams[i] = sorted(all_running_beams[i], key=lambda tup:np.prod(tup[1]), reverse=True)
+            all_running_beams[i] = all_running_beams[i][:max_beams]
         
-        batch_encoder_input = encode_sequences(input_encoding_dict, [text for _ in running_beams], max_input_length)
-        batch_decoder_input = np.asarray([i[0][0] for i in running_beams])
+        in_out_map = {}
+        batch_encoder_input = []
+        batch_decoder_input = []
+        t_c = 0
+        for text_i in all_running_beams:
+            if text_i not in in_out_map:
+                in_out_map[text_i] = []
+            for running_beam in all_running_beams[text_i]:
+                in_out_map[text_i].append(t_c)
+                t_c+=1
+                batch_encoder_input.append(texts[text_i])
+                batch_decoder_input.append(running_beam[0][0])
+
+
+        batch_encoder_input = encode_sequences(input_encoding_dict, batch_encoder_input, max_input_length)
+        batch_decoder_input = np.asarray(batch_decoder_input)
         batch_predictions = model.predict([batch_encoder_input, batch_decoder_input])
 
-        temp_running_beams = []
-        for running_beam_index, (running_beam, probs) in enumerate(running_beams):
-            if len(probs) >= min_cut_off_len:
-                completed_beams.append([running_beam[:,1:], probs])
-            else:
-                prediction = batch_predictions[running_beam_index]
-                sorted_args = prediction.argsort()
-                sorted_probs = np.sort(prediction)
+        t_c = 0
+        for text_i, t_cs in in_out_map.items():
+            temp_running_beams = []
+            for running_beam, probs in all_running_beams[text_i]:
+                if len(probs) >= min_cut_off_len:
+                    all_completed_beams[text_i].append([running_beam[:,1:], probs])
+                else:
+                    prediction = batch_predictions[t_c]
+                    sorted_args = prediction.argsort()
+                    sorted_probs = np.sort(prediction)
 
-                for i in range(1, beam_size+1):
-                    temp_running_beam = np.copy(running_beam)
-                    i = -1 * i
-                    ith_arg = sorted_args[:, i][len(probs)]
-                    ith_prob = sorted_probs[:, i][len(probs)]
-                    
-                    temp_running_beam[:, len(probs)] = ith_arg
-                    temp_running_beams.append([temp_running_beam, probs + [ith_prob]])
-                
-        running_beams = [b for b in temp_running_beams]
+                    for i in range(1, beam_size+1):
+                        temp_running_beam = np.copy(running_beam)
+                        i = -1 * i
+                        ith_arg = sorted_args[:, i][len(probs)]
+                        ith_prob = sorted_probs[:, i][len(probs)]
+                        
+                        temp_running_beam[:, len(probs)] = ith_arg
+                        temp_running_beams.append([temp_running_beam, probs + [ith_prob]])
 
-    return completed_beams
+                t_c+=1
 
-def infer(text, model, params, beam_size=3, max_beams=3, min_cut_off_len=10, cut_off_ratio=1.5):
+            all_running_beams[text_i] = [b for b in temp_running_beams]
+        
+        to_del = []
+        for i, v in all_running_beams.items():
+            if not v:
+                to_del.append(i)
+        
+        for i in to_del:
+            del all_running_beams[i]
+
+    return all_completed_beams
+
+def infer(texts, model, params, beam_size=3, max_beams=3, min_cut_off_len=10, cut_off_ratio=1.5):
+    if not isinstance(texts, list):
+        texts = [texts]
+
     input_encoding_dict = params['input_encoding']
     output_decoding_dict = params['output_decoding']
     max_input_length = params['max_input_length']
     max_output_length = params['max_output_length']
 
-    decoder_outputs = generate(text, input_encoding_dict, model, max_input_length, max_output_length, beam_size, max_beams, min_cut_off_len, cut_off_ratio)
+    all_decoder_outputs = generate(texts, input_encoding_dict, model, max_input_length, max_output_length, beam_size, max_beams, min_cut_off_len, cut_off_ratio)
     outputs = []
-    for decoder_output, probs in decoder_outputs:
-        outputs.append({'sequence': decode_sequence(output_decoding_dict, decoder_output[0]), 'prob': np.prod(probs)})
+
+    for i, decoder_outputs in all_decoder_outputs.items():
+        outputs.append([])
+        for decoder_output, probs in decoder_outputs:
+            outputs[-1].append({'sequence': decode_sequence(output_decoding_dict, decoder_output[0]), 'prob': np.prod(probs)})
 
     return outputs
 
@@ -255,13 +292,13 @@ def build_model(params_path = 'test/params', enc_lstm_units = 128, unroll = True
 if __name__ == '__main__':
     input_data = ['123', '213', '312', '321', '132', '231']
     output_data = ['123', '123', '123', '123', '123', '123']
-    build_params(input_data = input_data, output_data = output_data, params_path = 'test/params', max_lenghts=(10, 10))
+    build_params(input_data = input_data, output_data = output_data, params_path = 'params', max_lenghts=(10, 10))
     
-    model, params = build_model(params_path='test/params')
+    model, params = build_model(params_path='params')
 
     input_data, output_data = convert_training_data(input_data, output_data, params)
     
-    checkpoint = ModelCheckpoint('test/checkpoint', monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+    checkpoint = ModelCheckpoint('checkpoint', monitor='val_acc', verbose=1, save_best_only=True, mode='max')
     callbacks_list = [checkpoint]
 
     model.fit(input_data, output_data, validation_data=(input_data, output_data), batch_size=2, epochs=40, callbacks=callbacks_list)
